a047feba345586178edabd449239264d4a2e3517
Kate Bryan <kcbryan@email.unc.edu>
2021-01-25 13:48:49 -0500

    Finished Exercise 00!

---
+++ sandbox/zhcalc.py
@@ -0,0 +1,17 @@
+"""Some fun with chinese characters"""
+
+print("欢迎您来到的数学教室")
+零: int = 0
+一: int = 1
+二: int = 2
+三: int = 3
+四: int = 4
+五: int = 5
+六: int = 6
+七: int = 7
+八: int = 8
+九: int = 9
+
+print(二,零,二,一)
+
+
