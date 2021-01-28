975741df7021b80004d8b0aaae5ef72f39b17540
Kate Bryan <kcbryan@email.unc.edu>
2021-01-27 22:59:02 -0500

    random fun attempt to make my friend laugh

---
+++ sandbox/take_down_the_establishment.py
@@ -0,0 +1,10 @@
+"""a meme to help improve my dear friend's mood"""
+
+print("Welcome to Monty Python and the Holy Grail, UNC edition.")
+print("You have reached the Bridge of Death. Ye who wish to pass must answer me these question three, ere the other side you see!")
+
+name: str = input("What is your name? ")
+mood: str = input("What is your mood? ")
+classes: str = input("what do you wish to eliminate? " )
+
+print("O wise " + name + ", the Sakai pages for " + classes + " have been eliminated. Good luck on your quest!")
